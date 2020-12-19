8416fb4e3190c0287617ea39332c2ce4e48b9094
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2020-12-19 19:07:20 -0300

    19122020 - Inclusão Subscription.

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,5 @@
 import User from '../../../models/User';
+import { USER_ADDED } from './channels';
 
 export default {
     User: {
@@ -9,8 +10,21 @@ export default {
         user: (_, { id }) => User.findById(id),
     },
     Mutation: {
-        createUser: (_, { data }) => User.create(data),
+        createUser: async (_, { data }, { pubsub }) =>  { 
+            const user = await User.create(data);
+            
+            pubsub.publish(USER_ADDED, {
+                userAdded: user,
+            });
+
+            return user;
+        },
         updateUser: (_, {id, data}) => User.findOneAndUpdate(id, data, {new: true}),
         deleteUser: async (_, { id }) => !!(await User.findByIdAndDelete(id)),
+    },
+    Subscription: {
+        userAdded: {
+            subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED),
+        },
     },
 };
--- src/startServer.js
+++ src/startServer.js
@@ -1,10 +1,12 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, PubSub } from 'apollo-server';
 import mongoose from 'mongoose';
 
 function startServer({ typeDefs, resolvers }) {
 
     mongoose.connect('mongodb://10.11.0.4:27017/graphql', { useNewUrlParser:true});
-    const server = new ApolloServer({ typeDefs, resolvers});
+    
+    const pubsub = new PubSub();
+    const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub }});
     server.listen().then(() => console.log(`Server started at`));
 }
 
