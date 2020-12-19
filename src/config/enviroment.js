a01a0dbd95fc703344716a862035fc49e102ca31
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2020-12-19 20:33:04 -0300

    19122020 - Inclusão do arquivo configuração .dotenv.

---
+++ src/config/enviroment.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dbConnection:process.env.DB_CONNECTION,
+    dbHost:process.env.DB_HOST,
+    dbPort:process.env.DB_PORT,
+    dbDatabase:process.env.DB_DATABASE,
+    dbUsername:process.env.DB_USERNAME,
+    dbPassword:process.env.DB_PASSWORD,
+};
